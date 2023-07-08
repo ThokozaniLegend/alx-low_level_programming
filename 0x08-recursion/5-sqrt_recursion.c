1cb7057f51e6ad675471251694841088c2ba7ea8
ThokozaniLegend <teejey2@gmail.com>
2023-07-08 02:01:10 -0700

    corrections

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,10 +7,27 @@
  * Return: natural square root, or -1 if not natural root
  */
 
+int find_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: natural square root, or -1 if not natural root
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
 
-	return find_root(n, 0);
+	return (find_root(n, 0));
 }
